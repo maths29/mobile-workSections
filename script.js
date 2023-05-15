b99c2d6d4ba1d811a4bb143f8f591038c0e19469
mathsoft29 <iwuconcept@gmail.com>
2023-05-15 17:03:14 +0100

    Add a feature to index.html,style.css and javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "top-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "top-nav";
+    }
+  }
