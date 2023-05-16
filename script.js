6a1a306f0c73178797c6f84f0f430a57692bbe1a
mathsoft29 <iwuconcept@gmail.com>
2023-05-16 11:43:17 +0100

    uppdate the index.html and style.css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "top-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "top-nav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "top-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "top-nav";
   }
+}
