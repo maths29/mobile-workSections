cf3eea9583405740cfc007c3c3abb951d0ec452d
mathsoft29 <iwuconcept@gmail.com>
2023-05-18 15:45:39 +0100

    refactored the mobile nav menu in index.html,style.css and js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const mainListDiv = document.getElementById('mainListDiv');
-const mediaButton = document.getElementById('mediaButton');
-const navBar = document.getElementById('navbar');
-const bannerSec = document.getElementById('theSec');
-mediaButton.onclick = function () {
-  'use strict';
- mainListDiv.classList.toggle('show_list');
-  mediaButton.classList.toggle('active');
-navBar.classList.toggle('blur');
-bannerSec.classList.toggle('blur')
+const menuIcon = document.querySelector('.menuIcon');
+const nav = document.querySelector('.overlay-menu');
+const closeMenu = document.querySelector('#close-menu');
+const openLink = document.querySelectorAll('#open-link');
+const blurBg = document.querySelector('#blur-bg');
+
+menuIcon.addEventListener('click', () => {
+  nav.style.display = 'block';
+  blurBg.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+});
+
+closeMenu.addEventListener('click', () => {
+  nav.style.display = 'none';
+  blurBg.style.display = 'none';
+  document.body.style.overflow = 'auto';
+});
+
+openLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.style.display = 'none';
+    blurBg.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  });
+});
+
+window.onresize = () => {
+  if (window.innerWidth >= 768) {
+    nav.style.display = 'none';
+    blurBg.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
 };
