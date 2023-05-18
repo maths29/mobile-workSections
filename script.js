ca0603536c14c4a2460ede4dbbd143eac028ca84
mathsoft29 <iwuconcept@gmail.com>
2023-05-18 09:22:56 +0100

    Update index.html,style.css and js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const mainListDiv = document.getElementById('mainListDiv');
 const mediaButton = document.getElementById('mediaButton');
+const navBar = document.getElementById('navbar');
+const bannerSec = document.getElementById('theSec');
 mediaButton.onclick = function () {
   'use strict';
  mainListDiv.classList.toggle('show_list');
   mediaButton.classList.toggle('active');
+navBar.classList.toggle('blur');
+bannerSec.classList.toggle('blur')
 };
