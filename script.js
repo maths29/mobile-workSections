a34635e04a78f8b15bcd0b847d1c3bf0efee6d64
mathsoft29 <iwuconcept@gmail.com>
2023-05-20 00:13:03 +0100

    fix linters

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-const menuIcon = document.querySelector(".menuIcon");
-const nav = document.querySelector(".overlay-menu");
-const closeMenu = document.querySelector("#close-menu");
-const openLink = document.querySelectorAll("#open-link");
-const blurBg = document.querySelector("#blur-bg");
+const menuIcon = document.querySelector('.menuIcon');
+const nav = document.querySelector('.overlay-menu');
+const closeMenu = document.querySelector('#close-menu');
+const openLink = document.querySelectorAll('#open-link');
+const blurBg = document.querySelector('#blur-bg');
 
-menuIcon.addEventListener("click", () => {
-  nav.style.display = "block";
-  blurBg.style.display = "block";
-  document.body.style.overflow = "hidden";
+menuIcon.addEventListener('click', () => {
+  nav.style.display = 'block';
+  blurBg.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 });
 
-closeMenu.addEventListener("click", () => {
-  nav.style.display = "none";
-  blurBg.style.display = "none";
-  document.body.style.overflow = "auto";
+closeMenu.addEventListener('click', () => {
+  nav.style.display = 'none';
+  blurBg.style.display = 'none';
+  document.body.style.overflow = 'auto';
 });
 
 openLink.forEach((link) => {
-  link.addEventListener("click", () => {
-    nav.style.display = "none";
-    blurBg.style.display = "none";
-    document.body.style.overflow = "auto";
+  link.addEventListener('click', () => {
+    nav.style.display = 'none';
+    blurBg.style.display = 'none';
+    document.body.style.overflow = 'auto';
   });
 });
 
 window.onresize = () => {
   if (window.innerWidth >= 768) {
-    nav.style.display = "none";
-    blurBg.style.display = "none";
-    document.body.style.overflow = "auto";
+    nav.style.display = 'none';
+    blurBg.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 };
 
-const form = document.querySelector("form");
-const errorMessage = document.createElement("p");
-errorMessage.style.color = "red";
+const form = document.querySelector('form');
+const errorMessage = document.createElement('p');
+errorMessage.style.color = 'red';
 
 function validateEmail() {
   const emailInput = document.querySelector('input[type="email"]');
   const email = emailInput.value;
 
   if (email !== email.toLowerCase()) {
-    errorMessage.textContent = "Please enter the email address in lowercase.";
+    errorMessage.textContent = 'Please enter the email address in lowercase.';
     form.appendChild(errorMessage);
     return false;
   }
 
-  errorMessage.textContent = "";
+  errorMessage.textContent = '';
   return true;
 }
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   if (!validateEmail()) {
     event.preventDefault();
   }
