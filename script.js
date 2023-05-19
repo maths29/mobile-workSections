8ef4e7a0ac6bd2c9d29d02d90cf57b3f1242e2a2
mathsoft29 <iwuconcept@gmail.com>
2023-05-19 21:06:39 +0100

    Add form validation to the js file

--- script.js
+++ script.js
@@ -31,3 +31,33 @@ window.onresize = () => {
     document.body.style.overflow = 'auto';
   }
 };
+
+// Get the form element and the error message element
+const form = document.querySelector('form');
+const errorMessage = document.createElement('p');
+errorMessage.style.color = 'red';
+
+// Function to validate the email field
+function validateEmail() {
+  const emailInput = document.querySelector('input[type="email"]');
+  const email = emailInput.value;
+
+  // Check if the email is in lowercase
+  if (email !== email.toLowerCase()) {
+    errorMessage.textContent = 'Please enter the email address in lowercase.';
+    form.appendChild(errorMessage);
+    return false;
+  }
+
+  // If the email is in lowercase, remove any error message and allow form submission
+  errorMessage.textContent = '';
+  return true;
+}
+
+// Handle form submission
+form.addEventListener('submit', (event) => {
+  // Validate the email field
+  if (!validateEmail()) {
+    event.preventDefault(); // Prevent form submission
+  }
+});
