0f942dc7f4167128d8fc46802d219a9470e0210b
Briton Otieno <ojwangbriton@gmail.com>
2023-05-19 23:12:50 +0300

    Add js file update

--- script.js
+++ script.js
@@ -1,63 +1,57 @@
-const menuIcon = document.querySelector('.menuIcon');
-const nav = document.querySelector('.overlay-menu');
-const closeMenu = document.querySelector('#close-menu');
-const openLink = document.querySelectorAll('#open-link');
-const blurBg = document.querySelector('#blur-bg');
+const menuIcon = document.querySelector(".menuIcon");
+const nav = document.querySelector(".overlay-menu");
+const closeMenu = document.querySelector("#close-menu");
+const openLink = document.querySelectorAll("#open-link");
+const blurBg = document.querySelector("#blur-bg");
 
-menuIcon.addEventListener('click', () => {
-  nav.style.display = 'block';
-  blurBg.style.display = 'block';
-  document.body.style.overflow = 'hidden';
+menuIcon.addEventListener("click", () => {
+  nav.style.display = "block";
+  blurBg.style.display = "block";
+  document.body.style.overflow = "hidden";
 });
 
-closeMenu.addEventListener('click', () => {
-  nav.style.display = 'none';
-  blurBg.style.display = 'none';
-  document.body.style.overflow = 'auto';
+closeMenu.addEventListener("click", () => {
+  nav.style.display = "none";
+  blurBg.style.display = "none";
+  document.body.style.overflow = "auto";
 });
 
 openLink.forEach((link) => {
-  link.addEventListener('click', () => {
-    nav.style.display = 'none';
-    blurBg.style.display = 'none';
-    document.body.style.overflow = 'auto';
+  link.addEventListener("click", () => {
+    nav.style.display = "none";
+    blurBg.style.display = "none";
+    document.body.style.overflow = "auto";
   });
 });
 
 window.onresize = () => {
   if (window.innerWidth >= 768) {
-    nav.style.display = 'none';
-    blurBg.style.display = 'none';
-    document.body.style.overflow = 'auto';
+    nav.style.display = "none";
+    blurBg.style.display = "none";
+    document.body.style.overflow = "auto";
   }
 };
 
-// Get the form element and the error message element
-const form = document.querySelector('form');
-const errorMessage = document.createElement('p');
-errorMessage.style.color = 'red';
+const form = document.querySelector("form");
+const errorMessage = document.createElement("p");
+errorMessage.style.color = "red";
 
-// Function to validate the email field
 function validateEmail() {
   const emailInput = document.querySelector('input[type="email"]');
   const email = emailInput.value;
 
-  // Check if the email is in lowercase
   if (email !== email.toLowerCase()) {
-    errorMessage.textContent = 'Please enter the email address in lowercase.';
+    errorMessage.textContent = "Please enter the email address in lowercase.";
     form.appendChild(errorMessage);
     return false;
   }
 
-  // If the email is in lowercase, remove any error message and allow form submission
-  errorMessage.textContent = '';
+  errorMessage.textContent = "";
   return true;
 }
 
-// Handle form submission
-form.addEventListener('submit', (event) => {
-  // Validate the email field
+form.addEventListener("submit", (event) => {
   if (!validateEmail()) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
   }
 });
