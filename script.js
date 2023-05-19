bb67b0da7136a82693c712759123fd6b632bf84b
Briton Otieno <ojwangbriton@gmail.com>
2023-05-19 22:45:55 +0300

    Add local storage API to js file

--- script.js
+++ script.js
@@ -31,3 +31,31 @@ window.onresize = () => {
     document.body.style.overflow = 'auto';
   }
 };
+
+ // Retrieve data from local storage and fill the form
+ function fillFormData() {
+  const formData = JSON.parse(localStorage.getItem('contactFormData'));
+  if (formData) {
+    document.querySelector('#full-name').value = formData.name || '';
+    document.querySelector('#email').value = formData.email || '';
+    document.querySelector('#message').value = formData.message || '';
+  }
+}
+
+// Save form data to local storage
+function saveFormData() {
+  const name = document.querySelector('#full-name').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
+  const formData = { name, email, message };
+  localStorage.setItem('contactFormData', JSON.stringify(formData));
+}
+
+// Add event listeners to input fields
+const inputFields = document.querySelectorAll('#Contact input, #Contact textarea');
+inputFields.forEach((inputField) => {
+  inputField.addEventListener('input', saveFormData);
+});
+
+// Fill the form with data on page load
+fillFormData();
